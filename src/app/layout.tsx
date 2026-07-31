b3e620bcbed54a952e357885208acf88e70476b6
Baptiste <baptiste.ofm@gmail.com>
2026-07-31 14:10:50 +0000

    chore: force vercel redeploy with new dashboard design

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -92,3 +92,4 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
 }
 // Force rebuild Fri Jul 31 12:35:12 UTC 2026
 // rebuild Fri Jul 31 13:05:06 UTC 2026
+// Force rebuild 1785507050
